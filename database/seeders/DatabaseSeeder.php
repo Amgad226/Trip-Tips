79f5a1e46a0659042534a10a7988db90d268d2ef
Amgad226 <amgad.wr.1@gmail.com>
2022-07-01 18:18:41 +0300

    add defalte user by seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
         $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
