afd4cf5079f638d9ea0c365660c0eaa16c90bc4d
Amgad226 <amgad.wr.1@gmail.com>
2022-07-03 19:49:25 +0300

    reset pass

--- app/Http/Controllers/Api/loging/ForgetAndRestPass.php
+++ app/Http/Controllers/Api/loging/ForgetAndRestPass.php
@@ -21,7 +21,7 @@ class ForgetAndRestPass extends Controller
         if($user==null)
         return response()->json([
         'message'=>'user not exist',
-        'status'=>0,400  ]);
+        'status'=>0],400  );
 
         //انشأنا رمز عشوائي من 10 حروف 
         $token = Str::random(10);
@@ -41,14 +41,14 @@ class ForgetAndRestPass extends Controller
                 return response()->json([
                     'message'=>'check your email',
                     'status'=>1
-                ]);
+                ],200);
             }  
 
         catch(\Exception $e) 
             {
                 //يفضل نستخدم تراي وكاتش  وقت نستعمل ال 
                 //DB:table 
-                return response()->json(['message'=>$e->getMessage()]);
+                return response()->json(['message'=>$e->getMessage()],400);
             } }
 //_____________________________________________________________________________________/
   public function  reset(Request $request){
@@ -63,12 +63,12 @@ class ForgetAndRestPass extends Controller
 // dd($user);
         if( $user  ==null)
         {
-            return response()->json(['message '=>'user dosent exist',404]);
+            return response()->json(['message '=>'user dosent exist',400]);
         }
         //ازا كان الرمز يلي كاتبلليي ياه بل ريكويست غير يلي مخزن بل داتا بيز برضو باي 
         if($user->password_token!=$token)
         {
-            return response()->json(["message"=>'invalide token',403]);
+            return response()->json(["message"=>'invalide token'],400);
         }
         //ازا الشرطين ما تحققو
         //وقتا عدلي كلمة السر بل كلمة الجديدة يلي باعتلي ياها
@@ -77,7 +77,8 @@ class ForgetAndRestPass extends Controller
         //وساويلي الباسورد توكين نال مشان ما يضل يستخدم هاد الرمز ويغير كلمة السر كل شوي 
         DB::table('users')->where('email',$email)->update(['password_token' => null]);
 
-        return response()->json(['message'=>'success'],200);}
+        return response()->json(['message'=>'success'],200);
+    }
 
 
         
